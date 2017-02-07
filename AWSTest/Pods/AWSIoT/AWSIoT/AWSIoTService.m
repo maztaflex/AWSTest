f06edea8e367c395d569dea7f60e8ebd4798b519
Yongwhi <yonghwinam@smtown.com>
2017-02-07 16:57:18 +0900

    AWSLex pod 설치

--- AWSTest/Pods/AWSIoT/AWSIoT/AWSIoTService.m
+++ AWSTest/Pods/AWSIoT/AWSIoT/AWSIoTService.m
@@ -26,7 +26,7 @@
 #import "AWSIoTResources.h"
 
 static NSString *const AWSInfoIoT = @"IoT";
-static NSString *const AWSIoTSDKVersion = @"2.4.12";
+static NSString *const AWSIoTSDKVersion = @"2.4.16";
 
 
 @interface AWSIoTResponseSerializer : AWSJSONResponseSerializer
